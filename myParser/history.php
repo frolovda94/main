d02befed1009a05d9664fb30d62efde6a34ee995
frolovda94 <5_01@list.ru>
2014-10-08 05:33:07 +0400

    adding history
    get and post methods were added to the client side and php script which
    saves requests history was added to the server side.

--- myParser/history.php
+++ myParser/history.php
@@ -1,3 +1,18 @@
 <?php
-	echo "Hi!";
+
+header("Access-Control-Allow-Origin: *");
+date_default_timezone_set('Europe/Moscow');
+
+if($_GET){
+	echo file_get_contents("hist.txt");
+}
+else if($_POST){
+	$my_req = "IP: ".$_SERVER['REMOTE_ADDR']
+	.", time: ".date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME'])
+	.", expression: ".$_POST[message]."<br>"
+	.PHP_EOL;
+	$f = fopen("hist.txt","a");
+	fwrite($f,$my_req);
+	fclose($f);
+}
 ?>
