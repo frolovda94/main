0bc15b8b28da496d9d7c54bdb0032ae7030f7238
frolovda94 <5_01@list.ru>
2014-11-04 01:32:02 +0300

    server part update

--- myParser/history.php
+++ myParser/history.php
@@ -8,11 +8,28 @@ if($_GET){
 }
 else if($_POST){
 	$my_req = "IP: ".$_SERVER['REMOTE_ADDR']
-	.", time: ".date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME'])
-	.", expression: ".$_POST[message]."<br>"
-	.PHP_EOL;
+	.", time: ".date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']+60*60)
+	.", expression: ".$_POST[message]."<br>".PHP_EOL;
 	$f = fopen("hist.txt","a");
 	fwrite($f,$my_req);
 	fclose($f);
+
+	$replace_newline = str_replace(";", PHP_EOL, $_POST[tex]);
+
+	$g = fopen("tex.txt","a");
+	$new_note =
+	"\tDate: ".date('Y-m-d H:i:s', $_SERVER['REQUEST_TIME']+60*60)."\\\\*".PHP_EOL
+	."\tIP: ".$_SERVER['REMOTE_ADDR']."\\\\*".PHP_EOL
+	."\tExpression: ".$_POST[message]."\\\\*".PHP_EOL
+	.$replace_newline."\\par".PHP_EOL
+	."\t\\vspace{10 mm}".PHP_EOL;
+	fwrite($g, $new_note);
+	fclose($g);
+
+	$tex_final = fopen("tex_download.tex","w");
+	fwrite($tex_final , file_get_contents("tex_begin.txt"));
+	fwrite($tex_final , str_replace("^", "{\$^\\wedge\$}", file_get_contents("tex.txt")));
+	fwrite($tex_final , file_get_contents("tex_end.txt"));
+	fclose($tex_final);
 }
 ?>
